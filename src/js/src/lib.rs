b13aaafe6188b4d7d4dca5f648dc3ecfacfb79e2
Yang Bai <baiyang.001@bytedance.com>
2023-10-09 19:27:11 +0800

    impl 3

--- src/js/src/lib.rs
+++ src/js/src/lib.rs
@@ -1,16 +1,15 @@
-use wasm_bindgen::prelude::*;
-use zkwasm_rust_sdk::{wasm_input, require};
 use bytemuck::cast_slice;
+use wasm_bindgen::prelude::*;
+use zkwasm_rust_sdk::{require, wasm_input};
 
 static mut BOARD: [u32; 16] = [
-    1,0,1,0,
-    1,1,0,0,
-    1,0,0,1,
-    1,1,0,0,
+    1, 0, 1, 0,
+    1, 1, 0, 0,
+    1, 0, 0, 1,
+    1, 1, 0, 0,
 ];
 
 static mut CURRENCY: i32 = 20;
-
 
 #[wasm_bindgen]
 pub unsafe fn setBoard(index: usize, b: u32) {
@@ -34,14 +33,14 @@ pub unsafe fn getCurrency() -> i32 {
 
 unsafe fn random_fill() {
     let sum: u32 = BOARD.iter().sum();
-    let zero_count = BOARD.iter().filter(|&x| *x == 0) .count();
+    let zero_count = BOARD.iter().filter(|&x| *x == 0).count();
     // let min = BOARD.iter().min();
 
     if zero_count == 0 {
         return;
     }
 
-    let mut c = sum%(zero_count as u32);
+    let mut c = sum % (zero_count as u32);
 
     for i in 0..BOARD.len() {
         if BOARD[i] != 0 {
@@ -57,195 +56,241 @@ unsafe fn random_fill() {
     }
 }
 
-unsafe fn reward(k: i32) {
-    if  k > 4 {
+unsafe fn reward(k: u32) {
+    if k > 4 {
         CURRENCY = CURRENCY + (1 << (k - 4));
     }
 }
 
-fn left() {}
-fn right() {}
-fn top() {}
-fn bottom() {}
+unsafe fn left() {
+    for r in 0..4 {
+        let mut cur = r * 4;
 
-/*
-void left(void) {
-#pragma clang loop unroll(full)
-for (int r=0; r<4; r++) {
-int cur = r*4;
+        /* remove all zeros */
+        for i in 0..4 {
+            let current = r * 4 + i;
+            if BOARD[current] != 0 {
+                BOARD[cur] = BOARD[current];
+                cur += 1;
+            }
+        }
 
-/* remove all zeros */
-for (int i=0; i<4; i++) {
-int current = r*4+i;
-if (board[current]!=0) {
-board[cur] = board[current];
-cur = cur+1;
-}
-}
-for (; cur<r*4+4; cur++) {
-board[cur] = 0;
-}
+        loop {
+            if cur >= r * 4 + 4 {
+                break;
+            }
 
+            BOARD[cur] = 0;
+            cur += 1;
+        }
 
-/* patch pairs */
-cur = r*4;
-for (int s=0; s<4; ) {
-int current = r*4 + s;
-int next = current + 1;
-if (s!=3 && board[current] == board[next]) {
-int r = board[current];
-reward(r);
-board[cur] = r+1;
-++cur;
-s = s + 2;
-} else {
-board[cur] = board[current];
-++cur;
-s = s + 1;
-}
-}
-// Fill zero for the rest
-for (; cur<r*4+4; cur++) {
-board[cur] = 0;
-}
-}
-}
+        /* patch pairs */
+        cur = r * 4;
 
-void right(void) {
-#pragma clang loop unroll(full)
-for (int r=0; r<4; r++) {
-int cur = r*4+3;
+        let mut s = 0;
+        loop {
+            if s >= 4 {
+                break;
+            }
 
-/* remove all zeros */
-for (int i=0; i<4; i++) {
-int current = r*4+3-i;
-if (board[current]!=0) {
-board[cur] = board[current];
-cur = cur-1;
-}
-}
-for (; cur>=r*4; cur--) {
-board[cur] = 0;
+            let current = r * 4 + s;
+            let next = current + 1;
+            if s != 3 && BOARD[current] == BOARD[next] {
+                let r = BOARD[current];
+                reward(r);
+                BOARD[cur] = r + 1;
+                cur += 1;
+                s += 2;
+            } else {
+                BOARD[cur] = BOARD[current];
+                cur += 1;
+                s += 1;
+            }
+        }
+
+        // Fill zero for the rest
+        loop {
+            if cur >= r * 4 + 4 {
+                break;
+            }
+            BOARD[cur] = 0;
+            cur += 1;
+        }
+    }
 }
 
-/* patch pairs */
-cur = r*4 + 3;
-for (int s=3; s>=0; ) {
-int current = r*4+s;
-int next = current-1;
-if (s!=0 && board[current] == board[next]) {
-int r = board[current];
-reward(r);
-board[cur] = r+1;
-cur = cur - 1;
-s = s - 2;
-} else {
-board[cur] = board[current];
-cur = cur - 1;
-s = s - 1;
-}
-}
-// Fill zero for the rest
-for (; cur>=r*4; cur--) {
-board[cur] = 0;
-}
-}
+unsafe fn right() {
+    for r in 0..4 {
+        let mut cur = r * 4 + 3;
+
+        /* remove all zeros */
+        for i in 0..4 {
+            let current = r * 4 + 3 - i;
+            if BOARD[current] != 0 {
+                BOARD[cur] = BOARD[current];
+                cur -= 1;
+            }
+        }
+
+        loop {
+            if cur < r * 4 {
+                break;
+            }
+
+            BOARD[cur] = 0;
+            cur -= 1;
+        }
+
+        /* patch pairs */
+        cur = r * 4 + 3;
+        let mut s = 3;
+        loop {
+            if s < 0 {
+                break;
+            }
+
+            let current = r * 4 + s;
+            let next = current - 1;
+
+            if s != 0 && BOARD[current] == BOARD[next] {
+                let r = BOARD[current];
+                reward(r);
+                BOARD[cur] = r + 1;
+                cur -= 1;
+                s -= 2;
+            } else {
+                BOARD[cur] = BOARD[current];
+                cur -= 1;
+                s -= 1;
+            }
+        }
+
+        // Fill zero for the rest
+        loop {
+            if cur < r * 4 {
+                break;
+            }
+
+            BOARD[cur] = 0;
+            cur -= 1;
+        }
+    }
 }
 
-void top(void) {
-#pragma clang loop unroll(full)
-for (int c=0; c<4; c++) {
+unsafe fn top() {
+    for c in 0..4 {
+        let mut cur = c;
 
-int cur = c;
+        /* remove all zeros */
+        for i in 0..4 {
+            let current = i * 4 + c;
+            if BOARD[current] != 0 {
+                BOARD[cur] = BOARD[current];
+                cur += 4;
+            }
+        }
+        loop {
+            if cur >= 16 {
+                break;
+            }
+            BOARD[cur] = 0;
+            cur += 4;
+        }
 
-/* remove all zeros */
-for (int i=0; i<4; i++) {
-int current = i*4 + c;
-if (board[current]!=0) {
-board[cur] = board[current];
-cur = cur+4;
-}
-}
-while (cur < 16) {
-board[cur] = 0;
-cur = cur + 4;
-}
+        cur = c;
 
-cur = c;
-for (int s=0; s<4; ) {
-int current = s*4 + c;
-int next = current + 4;
-if (s!=3 && board[current] == board[next]) {
-int r = board[current];
-reward(r);
-board[cur] = r+1;
-cur = cur + 4;
-s = s + 2;
-} else {
-board[cur] = board[current];
-cur = cur + 4;
-s = s + 1;
-}
-}
-// Fill zero for the rest
-while (cur<16) {
-board[cur] = 0;
-cur = cur + 4;
-}
-}
-}
+        let mut s = 0;
+        loop {
+            if s >= 4 {
+                break;
+            }
 
-void bottom(void) {
-#pragma clang loop unroll(full)
-for (int c=0; c<4; c++) {
-int cur = 12 + c;
+            let current = s * 4 + c;
+            let next = current + 4;
 
-/* remove all zeros */
-for (int i=3; i>=0; i--) {
-int current = i*4 + c;
-if (board[current]!=0) {
-board[cur] = board[current];
-cur = cur-4;
-}
-}
-while (cur>=0) {
-board[cur] = 0;
-cur = cur - 4;
+            if s != 3 && BOARD[current] == BOARD[next] {
+                let r = BOARD[current];
+                reward(r);
+                BOARD[cur] = r + 1;
+                cur += 4;
+                s += 2;
+            } else {
+                BOARD[cur] = BOARD[current];
+                cur += 4;
+                s += 1;
+            }
+        }
+
+        loop {
+            if cur >= 16 {
+                break;
+            }
+
+            BOARD[cur] = 0;
+            cur += 4;
+        }
+    }
 }
 
+unsafe fn bottom() {
+    for c in 0..4 {
+        let mut cur = c + 12;
 
-/* patch pairs  */
-cur = 12 + c;
-for (int s=3; s>=0;) {
-int current = s*4 + c;
-int next = current - 4;
-if (s!=0 && board[current] == board[next]) {
-int r = board[current];
-reward(r);
-board[cur] = r+1;
-cur = cur - 4;
-s = s - 2;
-} else {
-board[cur] = board[current];
-cur = cur - 4;
-s = s - 1;
-}
-}
-while (cur >= 0) {
-board[cur] = 0;
-cur = cur - 4;
-}
-}
-}
+        /* remove all zeros */
+        for i in (0..4).rev() {
+            let current = i * 4 + c;
+            if BOARD[current] != 0 {
+                BOARD[cur] = BOARD[current];
+                cur -= 4;
+            }
+        }
+        loop {
+            if cur < 0 {
+                break;
+            }
+            BOARD[cur] = 0;
+            cur -= 4;
+        }
 
+        cur = c + 12;
 
+        let mut s = 3;
+        loop {
+            if s < 0 {
+                break;
+            }
 
- */
+            let current = s * 4 + c;
+            let next = current - 4;
+
+            if s != 0 && BOARD[current] == BOARD[next] {
+                let r = BOARD[current];
+                reward(r);
+                BOARD[cur] = r + 1;
+                cur -= 4;
+                s -= 2;
+            } else {
+                BOARD[cur] = BOARD[current];
+                cur -= 4;
+                s -= 1;
+            }
+        }
+
+        loop {
+            if cur < 0 {
+                break;
+            }
+
+            BOARD[cur] = 0;
+            cur -= 4;
+        }
+    }
+}
 
 
 #[wasm_bindgen]
 pub unsafe fn step(direction: u8) {
-    require(CURRENCY  >0);
+    require(CURRENCY > 0);
     CURRENCY -= 1;
 
     match direction {
@@ -288,7 +333,7 @@ pub unsafe fn zkmain() -> u64 {
                     let target_cell = *iter.next().unwrap();
                     sell(target_cell as usize);
                 }
-            },
+            }
             None => return 0,
         }
     }
